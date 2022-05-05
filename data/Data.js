3300fa26da0fff96b0e5df387328a3971f1b09ba
Ajay Savaliya <ajay.savaliya@techypanther.com>
2022-05-05 11:18:02 +0530

    update

--- data/Data.js
+++ data/Data.js
@@ -1,43 +1,4 @@
-import {Alert} from 'react-native';
-import {useDispatch} from 'react-redux';
-import {addUser, addUsers} from '../actions/actions';
-
-// export const onSave = ({
-//   name,
-//   email,
-//   gender,
-//   status,
-//   dispatch,
-//   refreshData,
-//   navigation,
-// }) => {
-//   const myHeaders = new Headers();
-
-//   myHeaders.append(
-//     'Authorization',
-//     'Bearer bc1e0809f9bb5ce03125ea49290ec9c8acc225870ebd21e484217e79b88800db',
-//   );
-//   myHeaders.append('Content-Type', 'application/json');
-//   fetch('https://gorest.co.in/public/v1/users', {
-//     method: 'POST',
-//     headers: myHeaders,
-//     body: JSON.stringify({
-//       name: name,
-//       email: email,
-//       gender: gender,
-//       status: status,
-//     }),
-//   })
-//     .then(response => response.json())
-//     .then(result => {
-//       dispatch(addUser(result));
-//       refreshData;
-//       navigation.navigate('User List');
-//     })
-//     .then(refreshData)
-
-//     .catch(error => console.log(error));
-// };
+import {addUsers} from '../actions/actions';
 
 export const getData = ({dispatch, setIsFetching}) => {
   const myHeaders = new Headers();
